b38c6d0667d988fb4c31089e08e4607cef477311
Ian Martorell <ianmartorell@gmail.com>
2020-04-07 10:49:01 +0200

    Add example

--- App.tsx
+++ App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Example from './Example';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+  return <Example />;
 }
 
 const styles = StyleSheet.create({
